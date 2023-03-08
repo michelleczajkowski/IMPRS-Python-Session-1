36525ae076e5e36cd32f6038fb8cdcb074a92f39
Michelle Czajkowski <michelle.czajkowski@ru.nl>
2023-03-08 10:59:20 +0100

    try to sync

--- lesson 1 test python.py
+++ lesson 1 test python.py
@@ -1,5 +1,9 @@
-print ("Hello, World!")
-if True:
-   print ("True")
-else:
-   print ("False")
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
