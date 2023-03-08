4e425f9cbe2f61e12e2f64944e168c5f390e8fb0
Michelle Czajkowski <michelle.czajkowski@ru.nl>
2023-03-08 11:11:53 +0100

    some junk from chatgps

--- lesson 1 test python.py
+++ lesson 1 test python.py
@@ -1,9 +1,25 @@
-one = 1
-two = 2
-three = one + two
-print(three)
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
 
-hello = "hello"
-world = "world"
-helloworld = hello + " " + world
-print(helloworld)
+# Create root window
+root = tk.Tk()
+
+# Hide the main window
+root.withdraw()
+
+# Show file dialog to choose file
+file_path = filedialog.askopenfilename(initialdir="C:/", title="Select a file", filetypes=[("Excel files", "*.xlsx"), ("CSV files", "*.csv")])
+
+# Print the selected file path
+print("Selected file:", file_path)
+
+# Read file using Pandas
+if file_path.endswith('.csv'):
+    df = pd.read_csv(file_path)
+else:
+    df = pd.read_excel(file_path)
+
+# Print file structure
+print("\nFile Structure:")
+print(df.head())
